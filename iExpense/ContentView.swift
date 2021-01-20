204ca1954dd6ade1154de7e4a48413fb2f3d99f7
Scott Obara <scott@rubbscreations.com>
2021-01-20 21:19:34 +1100

    Sheet and @Environment(\.presentationMode)

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,21 +7,29 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-}
-
-struct ContentView: View {
-    @ObservedObject private var user = User()
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
     
     var body: some View {
-        
         VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+            Text("Hello, \(name)")
+            Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+}
+ 
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button("Show Sheet") {
+            self.showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet){
+            SecondView(name: "Mr. Rubbs")
         }
     }
 }
