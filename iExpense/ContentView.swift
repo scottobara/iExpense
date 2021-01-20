bac2dd61158d458b556f76db0290e2063984b033
Scott Obara <scott@rubbscreations.com>
2021-01-20 21:07:53 +1100

    ObservableObject @Published properties and ObservedObject

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ContentView.swift
 //  iExpense
 //
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    @ObservedObject private var user = User()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
     }
 }
 
