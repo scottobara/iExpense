822794cd90a99d8cc74641eb136e0a9e486c337e
Scott Obara <scott@rubbscreations.com>
2021-01-23 17:50:48 +1100

    Day 38 Challenges completed

---
+++ iExpense/AddView.swift
@@ -0,0 +1,57 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Scott Obara on 21/1/21.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    @State private var showAlert = false
+
+    static let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+                    .foregroundColor(showAlert ? .red : .none)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing: Button("Save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                } else {
+                    showAlert = true
+                }
+            })
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Invalid Amount"), message: Text("Please enter enter whole numbers only for Amount"), dismissButton: .default(Text("Okay")){
+                    amount = ""
+                })
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        //AddView(expenses: Expenses())
+        ContentView()
+    }
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let id = UUID()
     let name: String
     let type: String
@@ -15,29 +15,65 @@ struct ExpenseItem: Identifiable {
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items: [ExpenseItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+
+        self.items = []
+    }
 }
 
 struct ContentView: View {
     @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+
+                        Spacer()
+                        Text("$\(item.amount)")
+                            .fontWeight(item.amount > 10 ? .bold : .none)
+                            .foregroundColor(item.amount > 100 ? .blue : .none)
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationTitle("iExpense")
-            .navigationBarItems(trailing:
+            .navigationBarItems(leading: EditButton(),
+                trailing:
                 Button(action: {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    self.expenses.items.append(expense)
+                    self.showingAddExpense = true
                 }) {
                     Image(systemName: "plus")
                 }
             )
+            
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: self.expenses)
+            
+            }
         }
     }
     
